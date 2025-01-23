c907dbcc414251319188277db5fd1560b1b70b03
Tan Zhi Xuan <69501009+Zhixuan0318@users.noreply.github.com>
2025-01-23 23:18:16 +0800

    update latest config and packages

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+    ...compat.config({
+        extends: ['next'],
+        settings: {
+            next: {
+                rootDir: 'packages/my-app/',
+            },
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+        },
+    }),
+];
+export default eslintConfig;
